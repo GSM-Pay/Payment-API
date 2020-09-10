378d0c49f51b0f90eab4460af796413dc508400a
unperknown <unperknown@icloud.com>
2020-09-10 17:56:42 +0900

    add: transactions queries

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -1,6 +1,29 @@
+import { transaction } from "../models";
+
 const resolver = {
     Query: {
+        // TO-DO: context 파라미터에서 인증 여부 검증
+        transactionsInBooth: (_: any, { bid }: any) => {
+            const transactions = transaction.findAll({
+                where: {
+                    bid: bid
+                }
+            });
 
+            return transactions;
+        },
+        transactionsInUser: () => {
+            // TO-DO: context 파라미터에서 인증된 유저의 PK 불러오기
+            const mockPid = 0;
+
+            const transcations = transaction.findAll({
+                where: {
+                    pid: mockPid
+                }
+            });
+
+            return transcations;
+        }
     },
     Mutation: {
 
--- src/typeDefs/query.ts
+++ src/typeDefs/query.ts
@@ -1,6 +1,8 @@
 import { gql } from 'apollo-server-koa';
 
 const query = gql`
+    transactionsInBooth(bid: Int): [Transaction]
+    transactionsInUser: [Transaction]
 `;
 
 export {
