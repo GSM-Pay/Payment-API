77c4cf3a217046e017dbd913bab1a53b21769467
unperknown <unperknown@icloud.com>
2020-09-11 10:21:10 +0900

    refactor: strict resolver type def

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -1,10 +1,11 @@
+import { IResolvers } from "apollo-server-koa";
 import { transaction } from "../models";
 
-const resolver = {
+const resolver: IResolvers = {
     Query: {
         // TO-DO: context 파라미터에서 인증 여부 검증
-        transactionsInBooth: (_: any, { bid }: any) => {
-            const transactions = transaction.findAll({
+        transactionsInBooth: async (_, { bid }, ctx) => {
+            const transactions = await transaction.findAll({
                 where: {
                     bid: bid
                 }
@@ -12,11 +13,11 @@ const resolver = {
 
             return transactions;
         },
-        transactionsInUser: () => {
+        transactionsInUser: async (_, __, ctx) => {
             // TO-DO: context 파라미터에서 인증된 유저의 PK 불러오기
             const mockPid = 0;
 
-            const transcations = transaction.findAll({
+            const transcations = await transaction.findAll({
                 where: {
                     pid: mockPid
                 }
