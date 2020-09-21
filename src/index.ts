a52462f5d1848849faf8d0701a3a800146ae6a60
startergate <startergate36@gmail.com>
2020-09-21 10:49:30 +0900

    misc: remove unused router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // import Koa.js related package
 import * as Koa from 'koa';
-import * as Router from 'koa-router';
 import * as bodyParser from 'koa-bodyparser';
 
 import { ApolloServer } from 'apollo-server-koa';
