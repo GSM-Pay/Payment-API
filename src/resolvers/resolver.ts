e5650d907921c3fb79aaa23b59572c2f2b376760
startergate <startergate36@gmail.com>
2020-09-21 09:29:11 +0900

    feat: 로그인

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -1,9 +1,37 @@
+import * as crypto from 'crypto';
+
+import * as redis from 'redis';
+import * as uuid from 'uuid';
+
+import { user } from '../models';
+
+const client = redis.createClient({
+    host: process.env.REDIS_HOST
+});
+
 const resolver = {
     Query: {
 
     },
     Mutation: {
+        Login: async (_: any, {id, pw}: any, ctx: any) => {
+            const data = ctx.request.body;
+            const hash = crypto.createHash('sha512');
+            hash.update(data.pw);
+            data.pw = hash.digest('hex');
 
+            const result = await user.findOne({ where: data, attributes: ['pid'] });
+
+            ctx.assert(result, 400);
+
+            const session = uuid.v4();
+
+            if (!(await client.set(session, result.pid.toString()))) {
+                ctx.throw(500);
+            }
+
+            return {data: session}
+        }
     }
 };
 
--- src/typeDefs/mutation.ts
+++ src/typeDefs/mutation.ts
@@ -1,6 +1,9 @@
 import { gql } from 'apollo-server-koa';
 
 const mutation = gql`
+    type Mutation {
+        Login(id: String, pw: String): Session
+    }
 `;
 
 
