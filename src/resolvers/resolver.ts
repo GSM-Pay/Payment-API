f4dc249a733fdf87a64c7e326e3b37baa7efd8c0
Choi-Yeonjin <yeonjin9721@naver.com>
2020-09-21 09:31:44 +0900

    add: 환불하기

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -1,8 +1,23 @@
+import { transaction, user } from '../models'
+
 const resolver = {
     Query: {
 
     },
     Mutation: {
+        refund: async(_:any, { tid }: any) => {
+            const destroyId = await transaction.findOne({ where: {
+                tid:tid
+            }
+                
+            })
+            await transaction.destroy({ where: {
+                tid : tid
+            }
+            })
+
+            return destroyId
+        }
 
     }
 };
